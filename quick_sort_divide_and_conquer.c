da58c421948afe139feee89ca5466d9ccc3e71e4
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2025-04-13 02:17:19 +0530

    Update and rename main.c to quick_sort_divide_and_conquer.c

--- quick_sort_divide_and_conquer.c
+++ quick_sort_divide_and_conquer.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    int temp = a;
-    a=b;
-    b=temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
-    int i = low-1;
+    int i = low - 1;
 
-    for(int j = low; j<high; j++)
+    for (int j = low; j < high; j++)
     {
         if (arr[j] < pivot)
         {
             i++;
-            swap(arr[i], arr[j]);
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(arr[i+1], arr[high]);
-    return i+1;
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
 }
 
 void quickSort(int arr[], int low, int high)
@@ -29,8 +29,8 @@ void quickSort(int arr[], int low, int high)
     if (low < high)
     {
         int pi = partition(arr, low, high);
-        quickSort(arr, low, pi-1);
-        quickSort(arr, pi+1, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
@@ -38,21 +38,18 @@ int main()
 {
     int n;
 
-    // Taking input for array size
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Taking input for array elements
     printf("Enter %d elements: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    quickSort(arr, 0, n-1);
+    quickSort(arr, 0, n - 1);
 
-    // Printing sorted array
     printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
