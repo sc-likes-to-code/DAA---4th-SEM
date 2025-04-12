c65979efa6cb817ff16cc4594d6e95f9725a7727
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2025-04-13 02:16:15 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void swap(int a, int b)
+{
+    int temp = a;
+    a=b;
+    b=temp;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low-1;
+
+    for(int j = low; j<high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+int main()
+{
+    int n;
+
+    // Taking input for array size
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Taking input for array elements
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, n-1);
+
+    // Printing sorted array
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
