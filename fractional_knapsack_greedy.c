cfcf88fc0eef1fc66ecb81671d5cb618247cfc09
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2025-04-28 18:03:45 +0530

    Update fractional_knapsack_greedy.c
    done

--- fractional_knapsack_greedy.c
+++ fractional_knapsack_greedy.c
@@ -1,54 +1,81 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void swapFloat(float *a, float *b) {
-    float temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void swapInt(int *a, int *b) {
+void swapInt(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void sortByRatio(float ratio[], int weight[], int value[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (ratio[j] < ratio[j+1]) {
-                swapFloat(&ratio[j], &ratio[j+1]);
-                swapInt(&weight[j], &weight[j+1]);
-                swapInt(&value[j], &value[j+1]);
+void swapDouble(double *a, double *b)
+{
+    double temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortItems(int weights[], int profits[], double ratio[], int n)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            if (ratio[j+1] > ratio[j])
+            {
+                swapDouble(&ratio[j],&ratio[j+1]);
+                swapInt(&weights[j], &weights[j+1]);
+                swapInt(&profits[j], &profits[j+1]);
             }
         }
     }
 }
 
-int main() {
-    int n = 3;
-    int weight[] = {10, 20, 30};
-    int value[]  = {60, 100, 120};
-    int W = 50;
+double fractionalKnapsack(int weights[], int profits[], double ratio[], int n, int capacity)
+{
+    sortItems(weights, profits, ratio, n);
+    double totalProfit = 0.0;
 
-    float ratio[n];
-    for (int i = 0; i < n; i++) {
-        ratio[i] = (float)value[i] / weight[i];
-    }
-
-    sortByRatio(ratio, weight, value, n);
-
-    float totalValue = 0.0;
-    for (int i = 0; i < n && W > 0; i++) {
-        if (weight[i] <= W) {
-            totalValue += value[i];
-            W -= weight[i];
-        } else {
-            totalValue += ratio[i] * W;
-            W = 0;
+    for (int i = 0; i < n && capacity > 0; i++)
+    {
+        if (weights[i] <= capacity)
+        {
+            totalProfit += profits[i];
+            capacity -= weights[i];
+        }
+        else
+        {
+            totalProfit += profits[i] * ((double)capacity/ weights[i]);
+            capacity = 0;
         }
     }
 
-    printf("Maximum value in knapsack: %.2f\n", totalValue);
+    return totalProfit;
+}
+
+int main()
+{
+    int n;
+    double capacity;
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    printf("Enter capacity of knapsack: ");
+    scanf("%lf", &capacity);
+
+    int weights[n], profits[n];
+    double ratio[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter weight and profit for item %d: ", i+1);
+        scanf("%d%d", &weights[i], &profits[i]);
+        ratio[i] = (double) profits[i]/ weights[i];
+    }
+
+    double maxProfit = fractionalKnapsack(weights, profits, ratio, n, capacity);
+
+    printf("\nMaximum profit = %.2lf\n", maxProfit);
 
     return 0;
 }
